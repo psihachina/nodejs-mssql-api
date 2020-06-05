71321d11c09df503637b751be4dfb2124d4d17e9
PSiHachina <psihachina@gmail.com>
2020-06-05 11:19:32 +0300

    add cors

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ var bodyParser = require('body-parser');
 var app = express();
 var server = require('http').createServer(app);
 var protectJSON = require('./lib/protectJSON');
+const cors = require("cors");
+
+
+app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 
 
 app.use(express.static(__dirname + '/public'));
